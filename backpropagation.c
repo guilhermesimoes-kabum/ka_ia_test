73c6b83602f1c05f71d7d5f8dd6d5c5f1ccc2705
guilhermesimoes-kabum <guilherme.simoes@kabum.com.br>
2023-02-22 17:17:05 -0300

    initial backpropagation

--- backpropagation.c
+++ backpropagation.c
@@ -7,11 +7,17 @@ float derivate(float out) {
     return out * (1 - out);
 }
 
-float backpropagation(
+float *backpropagation(
     float* weights, 
     float* bias, 
     float cost,
     float output
 ) {
+    float new_weights[SIZE(weights)];
 
+    for(uint32_t i = 0; i < SIZE(weights); i++) {
+        *(new_weights + i) = *(weights + i) - derivate(output) * ;
+    }
+
+    return new_weights;
 }
